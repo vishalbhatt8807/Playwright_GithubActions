f3f304b6e1e9e633ec79315bd90b1e87d6d9284f
Vishal <vishal.bhatt8807@gmail.com>
2025-10-01 06:37:00 +0530

    push all code

---
+++ tests/home.spec.ts
@@ -0,0 +1,16 @@
+import {test,expect} from '@playwright/test';
+
+test.beforeEach(async({page})=>{
+   await page.goto("https://www.flipkart.com/");
+})
+
+test('Perform search Operation', async({page}) => {
+    await page.getByPlaceholder("Search for Products, Brands and More").click();
+    await page.keyboard.down('Enter');
+    await page.getByRole('link',{name:'t shirts'}).click();
+    await expect(await page.getByPlaceholder("Search for Products, Brands and More").getAttribute('value')).toBe("t shirts")
+    await page.waitForTimeout(5000);
+    const product = (await page.$$("//div[@class='_75nlfW']/div")).length;
+    console.log(product);
+    expect(product).toBe(40);
+});
